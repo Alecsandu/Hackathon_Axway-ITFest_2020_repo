5964592b542b1fcb289bc391eeb5bbc0859d22a6
Vlad Masgras <masgrasvlad@gmail.com>
2020-11-07 10:24:24 +0200

    MongoDB connection

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const connectDB = require('./config/db')
 
 const app = express();
+
+connectDB()
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/styles"));
