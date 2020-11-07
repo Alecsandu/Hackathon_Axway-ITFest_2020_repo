6fe8271c5e1e96f4e38b7cb830a940ca471e5e25
Alecsandu <45858741+Alecsandu@users.noreply.github.com>
2020-11-07 14:47:23 +0200

    Added login signin features

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
+app.get("/signin", function (req, res) {
+    res.render("signin");
+});
+
 app.use(function (req, res) {
   res.status(404).render("error");
 });
